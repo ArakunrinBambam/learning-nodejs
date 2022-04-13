7022a957f75a321da855517daf0b4f5616bc655e
Bamidele Adedotun O <adedotunolusegun@yahoo.com>
2022-04-13 16:19:40 +0100

    Express module added instead of using normal http module

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-
-// const helpers = require('./helper.js');
- const { sum } = require('./helper.js');
-
- const http = require('http');
-
- const server = http.createServer((req, res) => {
-
- 	res.end("Hello world from node js server updated");
-
- });
+const express = require('express');
 
 
- server.listen(3000)
-
-// const total = helpers.sum(10,200);
-const total = sum(30,70)
-
-console.log("Total: ",total);
+const app = express();
 
 
+app.get('/', function(req,res){
+
+	res.send("Hello World from express")
+	
+})
+
+
+app.listen(3000);
